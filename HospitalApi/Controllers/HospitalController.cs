a79365f559e987064ecc96ab67b0d330c28b9fe0
Vsevolod <rusakevich.ceva@yandex.ru>
2024-03-10 16:10:22 +0300

    Добавьте файлы проекта.

---
+++ HospitalApi/Controllers/HospitalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalApi.Controllers
+{
+    public class Product
+    {
+        public string NameNameMedicine { get; set; }
+        public string Warehouse { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class HospitalController : Controller
+    {
+        [HttpGet, Route("Apteka")]
+        public IEnumerable<Product> GetTableData()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product { NameNameMedicine = "Аджисепт", Warehouse = "Склад в поле", Value = 100 });
+            products.Add(new Product { NameNameMedicine = "Грамидин", Warehouse = "Склад в городе", Value = 400 });
+            products.Add(new Product { NameNameMedicine = "Гексарал спрей", Warehouse = "Склад за городом", Value = 250 });
+            products.Add(new Product { NameNameMedicine = "Альмагель А", Warehouse = "Склад за городом", Value = 250 });
+
+            return products;
+        }
+    }
+}
